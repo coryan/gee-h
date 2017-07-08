a0b15b191f124e98f7ea901f47b20ed04cc3c629
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-07-08 12:28:11 -0400

    Test the revoke vs. timer race condition.

--- gh/detail/session_impl_ut.cpp
+++ gh/detail/session_impl_ut.cpp
@@ -226,6 +226,76 @@ TEST(session_impl, full_lifecycle) {
   EXPECT_NO_THROW(session.reset(nullptr));
 }
 
+/**
+ * @test Verify that gh::detail::session_impl handles races between revoke() and the timer.
+ */
+TEST(session_impl, race_timer) {
+  using namespace std::chrono_literals;
+  using namespace gh::detail;
+
+  completion_queue_type queue;
+  prepare_mocks_common(queue);
+  using namespace ::testing;
+  // ... we expect a call to request a lease ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "session/preamble/lease_grant";
+  }))).WillOnce(Invoke([](auto bop) {
+    using op_type = gh::detail::async_rpc_op<etcdserverpb::LeaseGrantRequest, etcdserverpb::LeaseGrantResponse>;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    // ... verify the request is what we expect ...
+    EXPECT_EQ(op->request.ttl(), 5);
+    EXPECT_EQ(op->request.id(), 0);
+
+    // ... in this test we just assume everything worked, so provide a response ...
+    op->response.set_error("");
+    op->response.set_id(1000);
+    op->response.set_ttl(42);
+    // ... and to not forget the callback ...
+    bop->callback(*bop, true);
+  }));
+
+  // ... and a call to setup a timer ...
+  std::shared_ptr<deadline_timer> pending_timer;
+  auto handle_timer = [r = std::ref(pending_timer)](auto bop) {
+    using op_type = gh::detail::deadline_timer;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    // ... the standard trick to downcast shared_ptr<> ...
+    r.get() = std::shared_ptr<deadline_timer>(bop, op);
+  };
+  EXPECT_CALL(*queue.interceptor().shared_mock, make_deadline_timer(Truly([](auto op) {
+    return op->name == "session/set_timer/ttl_refresh";
+  }))).WillRepeatedly(Invoke(handle_timer));
+
+  auto session = std::make_unique<session_type>(queue, std::unique_ptr<etcdserverpb::Lease::Stub>(), 5000ms);
+  EXPECT_EQ(session->lease_id(), 1000UL);
+  EXPECT_EQ(session->actual_TTL().count(), 42000);
+  ASSERT_TRUE(session->is_active());
+
+  ASSERT_TRUE((bool)pending_timer);
+
+  // ... prepare to hold the LeaseRevoke() call ...
+  using revoke_op_type = async_rpc_op<etcdserverpb::LeaseRevokeRequest, etcdserverpb::LeaseRevokeResponse>;
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "session/revoke/lease_revoke";
+  }))).WillOnce(Invoke([t = std::ref(pending_timer), &session](auto bop) {
+    ASSERT_FALSE(session->is_active());
+    auto* op = dynamic_cast<revoke_op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    // ... fire the timer, which should not create a new timer as it usual does ...
+    auto p = std::move(t.get());
+    p->callback(*p, true);
+    // ... the timer should not reconnect ...
+    ASSERT_FALSE((bool)t.get());
+    bop->callback(*bop, true);
+  }));
+
+  EXPECT_NO_THROW(session->revoke());
+  ASSERT_FALSE(session->is_active());
+  EXPECT_NO_THROW(session.reset(nullptr));
+}
+
 namespace {
 void prepare_mocks_common(completion_queue_type& queue) {
   using namespace ::testing;
