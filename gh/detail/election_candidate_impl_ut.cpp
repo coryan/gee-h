28eac14edd16bdbb86256abe26676cca4f0d67cc
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-07-30 18:05:31 -0400

    Improve coverage in create_node().

--- gh/detail/election_candidate_impl_ut.cpp
+++ gh/detail/election_candidate_impl_ut.cpp
@@ -498,6 +498,79 @@ TEST(election_candidate_impl, existing_node_value_mismatch) {
   EXPECT_NO_THROW(candidate.reset(nullptr));
 }
 
+/// @test verify gh::detail::election_candidate_impl<> works if the node exists and it cannot be modified.
+TEST(election_candidate_impl, existing_node_put_error) {
+  // gh::log::instance().add_sink(
+  //    gh::make_log_sink([](gh::severity sev, std::string&& msg) { std::cout << msg << std::endl; }));
+  using namespace std::chrono_literals;
+  using namespace gh::detail;
+
+  completion_queue_type queue;
+
+  using namespace ::testing;
+  // We are going to simulate an election where the candidate has to wait for another leader.  Several calls are
+  // trivial for this purpose ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_create_rdwr_stream(_)).WillRepeatedly(Invoke([](auto op) {
+    op->callback(*op, true);
+  }));
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_writes_done(_)).WillRepeatedly(Invoke([](auto op) {
+    op->callback(*op, true);
+  }));
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_finish(_)).WillRepeatedly(Invoke([](auto op) {
+    op->callback(*op, true);
+  }));
+
+  // ... verify that the candidate makes a valid query to create the node, in this case we respond with a failed
+  // creation and with a different existing value that we desire ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "election_candidate/create_node";
+  }))).WillOnce(Invoke([](auto bop) {
+    using op_type = async_rpc_op<etcdserverpb::TxnRequest, etcdserverpb::TxnResponse>;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+
+    // ... the node already exists ...
+    op->response.set_succeeded(false);
+    op->response.mutable_header()->set_revision(2000);
+    // ... but we are lucky, the value in the node is exactly the same we want to insert ...
+    auto& range = *op->response.add_responses()->mutable_response_range();
+    auto& kv = *range.add_kvs();
+    kv.set_value("bcd1200");
+    kv.set_key("mock-election/123456");
+    kv.set_create_revision(1500);
+    bop->callback(*bop, true);
+  }));
+
+  // ... because the value was mismatched we expect a call to publish_value(), simulate an error ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "election_candidate/publish_value";
+  }))).WillOnce(Invoke([](auto bop) {
+    using op_type = async_rpc_op<etcdserverpb::TxnRequest, etcdserverpb::TxnResponse>;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+
+    ASSERT_EQ(op->request.success_size(), 1);
+    ASSERT_TRUE(op->request.success(0).has_request_put());
+    EXPECT_EQ(op->request.success(0).request_put().key(), "mock-election/123456");
+    EXPECT_EQ(op->request.success(0).request_put().value(), "bcd2000");
+    // ... this indicates that either some external changes to the etcd server state took place, or maybe permissions
+    // changed, or maybe a lease expired?  In any case, very rare errors, we just want to make sure they are reported
+    // correctly ...
+    op->response.set_succeeded(false);
+    bop->callback(*bop, true);
+  }));
+
+  auto candidate = std::make_unique<candidate_type>(
+    queue, 0x123456, std::unique_ptr<etcdserverpb::KV::Stub>(), std::unique_ptr<etcdserverpb::Watch::Stub>(),
+    "mock-election", "bcd2000");
+
+  ASSERT_THROW(candidate->campaign(), std::exception);
+  EXPECT_FALSE(candidate->elected());
+
+  EXPECT_CALL(*queue.interceptor().shared_mock, try_cancel()).Times(1);
+  EXPECT_NO_THROW(candidate.reset(nullptr));
+}
+
 /// @test verify election_candidate_impl<> can handle a compaction when setting up watchers.
 TEST(election_candidate_impl, compaction_before_watch) {
   // gh::log::instance().add_sink(
