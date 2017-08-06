e800db61cc44353ee1ae638ef72166902c4c2d1e
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-08-06 17:42:09 -0400

    Use python3 because it has better subprocess implementation.

--- gh/integration_tests.py
+++ gh/integration_tests.py
@@ -1,39 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import os
 import socket
 import subprocess
 import time
 import unittest
 
+
 class Test(unittest.TestCase):
     @classmethod
-    def tryConnect(cls, sleep, niter = 10):
+    def tryConnect(cls, sleep, niter=10):
         for _ in range(0, niter):
+            time.sleep(sleep)
             sleep = sleep * 2
-            time.sleep(1)
             try:
                 _ = socket.create_connection(("localhost", "22379"), 0.5)
-                print "etcd server is accepting connections"
+                print("etcd server is accepting connections")
                 return True
             except socket.timeout:
                 continue
             except socket.error:
-                print " .. connecting to etcd failed"
+                print(" .. connecting to etcd failed")
         return False
 
     @classmethod
     def setUpClass(cls):
         if cls.tryConnect(0.01, 3):
             return
-        print "initial connection failed, starting new etcd server instance"
-        wnull = open(os.devnull, 'w')
+        print("initial connection failed, starting new etcd server instance")
         cls._etcd = subprocess.Popen(
             ["/usr/bin/etcd", "--listen-client-urls", "http://localhost:22379", "--advertise-client-urls",
-             "http://localhost:22379"], stderr=wnull, stdout=wnull)
-        if cls.tryConnect(0.05):
-           return
-        raise SystemExit("cannot connect nor start+connect the etcd server")
+             "http://localhost:22379"], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        if not cls.tryConnect(0.05):
+            raise SystemExit("cannot connect nor start+connect the etcd server")
 
     @classmethod
     def tearDownClass(cls):
@@ -49,22 +47,18 @@ class Test(unittest.TestCase):
 
     def test_session(self):
         try:
-            text = subprocess.check_output(
-                ["./gh_session_test"],
-                stderr=subprocess.STDOUT)
-            print text
+            text = subprocess.check_output(["./gh_session_test"], stderr=subprocess.STDOUT)
+            print(text)
         except subprocess.CalledProcessError as ex:
-            print ex.output
+            print(ex.output)
             self.assertEqual(ex.returncode, 0)
 
     def test_leader_election(self):
         try:
-            text = subprocess.check_output(
-                ["./gh_leader_election_test"],
-                stderr=subprocess.STDOUT)
-            print text
+            text = subprocess.check_output(["./gh_leader_election_test"], stderr=subprocess.STDOUT)
+            print(text)
         except subprocess.CalledProcessError as ex:
-            print ex.output
+            print(ex.output)
             self.assertEqual(ex.returncode, 0)
 
 if __name__ == '__main__':
