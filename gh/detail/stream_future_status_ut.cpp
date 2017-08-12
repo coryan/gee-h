8af9ef57cb61bf2c962ac46583cf8d3d83b93dd4
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-08-12 09:09:10 -0400

    Improve coverage for tests.

--- gh/detail/stream_future_status_ut.cpp
+++ gh/detail/stream_future_status_ut.cpp
@@ -7,4 +7,7 @@ TEST(stream_future_status, basic) {
   std::ostringstream os;
   os << std::future_status::deferred << " " << std::future_status::ready << " " << std::future_status::timeout;
   EXPECT_EQ(os.str(), "[deferred] [ready] [timeout]");
+
+  auto invalid = (std::future_status)-1;
+  EXPECT_THROW(os << invalid, std::exception);
 }
