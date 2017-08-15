8f34e2136cb2aae4f027c2da0b8320c3101b28b5
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-08-14 21:50:40 -0400

    Improve code coverage.

--- gh/detail/cluster_membership_impl_ut.cpp
+++ gh/detail/cluster_membership_impl_ut.cpp
@@ -64,6 +64,9 @@ TEST(cluster_membership_impl, normal_lifecycle) {
   EXPECT_THAT(urls, ElementsAre("url0.0", "url0.1", "url1.0", "url1.1"));
 
   EXPECT_NO_THROW(cluster.shutdown());
+  pt = std::move(pending_timer);
+  ASSERT_TRUE(pt);
+  pt->callback(*pt, false);
 }
 
 /// @test Verify that cluster membership works correctly even with some failures.
@@ -154,4 +157,104 @@ TEST(cluster_membership_impl, with_failure) {
   pt->callback(*pt, true);
 
   EXPECT_NO_THROW(cluster.shutdown());
+}
+
+/// @test Verify that cluster membership works correctly even when one of the cluster members returns an empty list.
+TEST(cluster_membership_impl, with_empty_list) {
+  using namespace std::chrono_literals;
+  using namespace gh::detail;
+  using namespace testing;
+
+  using namespace std::chrono_literals;
+  completion_queue_type queue;
+  std::shared_ptr<grpc::ChannelCredentials> credentials;
+  auto backoff = std::make_shared<gh::detail::exponential_backoff>(10ms, 24h, 1000);
+  cluster_membership_type cluster(queue, credentials, backoff, "localhost:22379", 30s);
+
+  std::shared_ptr<deadline_timer> pending_timer;
+  auto handle_timer = [r = std::ref(pending_timer)](auto bop) {
+    auto* op = dynamic_cast<deadline_timer*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    // ... the standard trick to downcast shared_ptr<> ...
+    r.get() = std::shared_ptr<deadline_timer>(bop, op);
+  };
+  EXPECT_CALL(*queue.interceptor().shared_mock, make_deadline_timer(Truly([](auto op) {
+    return op->name == "cluster_membership/refresh_timer";
+  }))).WillRepeatedly(Invoke(handle_timer));
+
+  // ... we expect a call to request a lease ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  }))).WillOnce(Invoke([](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+
+    // ... in this test we just assume everything worked, so provide a response ...
+    auto& m0 = *op->response.add_members();
+    *m0.add_clienturls() = std::string("url0.0");
+    *m0.add_clienturls() = std::string("url0.1");
+    *m0.add_clienturls() = std::string("url0.2");
+    // ... and to not forget the callback ...
+    bop->callback(*bop, true);
+  }));
+
+  EXPECT_NO_THROW(cluster.startup());
+  auto pt = std::move(pending_timer);
+  ASSERT_TRUE(pt);
+  pt->callback(*pt, true);
+
+  auto urls = cluster.current_urls();
+  EXPECT_THAT(urls, ElementsAre("url0.0", "url0.1", "url0.2"));
+
+  // ... simulate an empty list in the results ...
+  auto& call0 = EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  })));
+  call0.WillOnce(Invoke([](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    bop->callback(*bop, true);
+  }));
+  // ... try again with a shorter list ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  }))).After(call0).WillOnce(Invoke([&cluster](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+
+    // ... the collection should have changed as the previous callback executed ...
+    auto urls = cluster.current_urls();
+    EXPECT_THAT(urls, ElementsAre("url0.0", "url0.1", "url0.2"));
+
+    // ... in this test we just assume everything worked, so provide a response ...
+    auto& m0 = *op->response.add_members();
+    *m0.add_clienturls() = std::string("url1.0");
+    *m0.add_clienturls() = std::string("url1.1");
+    // ... and to not forget the callback ...
+    bop->callback(*bop, true);
+  }));
+
+  pt = std::move(pending_timer);
+  ASSERT_TRUE(pt);
+  pt->callback(*pt, true);
+  urls = cluster.current_urls();
+  EXPECT_THAT(urls, ElementsAre("url1.0", "url1.1"));
+
+  EXPECT_NO_THROW(cluster.shutdown());
+}
+
+/// @test Verify that cluster membership raises the right exceptions when provided with invalid arguments.
+TEST(cluster_membership_impl, invalid_arguments) {
+  using namespace std::chrono_literals;
+  using namespace gh::detail;
+  using namespace testing;
+
+  using namespace std::chrono_literals;
+  completion_queue_type queue;
+  std::shared_ptr<grpc::ChannelCredentials> credentials;
+  auto backoff = std::make_shared<gh::detail::exponential_backoff>(10ms, 24h, 1000);
+  ASSERT_THROW(cluster_membership_type(queue, credentials, backoff, "localhost:22379", 500ms), std::exception);
 }
