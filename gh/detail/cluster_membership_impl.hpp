45e629ee7231681b4bcca8ae73ff3de8d638ba38
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-08-14 21:21:30 -0400

    Implement periodic refresh in cluster_membership_impl.
    And also start on test coverage.

--- gh/detail/cluster_membership_impl.hpp
+++ gh/detail/cluster_membership_impl.hpp
@@ -1,13 +1,16 @@
 #ifndef gh_detail_cluster_membership_impl_hpp
 #define gh_detail_cluster_membership_impl_hpp
 
+#include <gh/completion_queue.hpp>
 #include <gh/detail/async_rpc_op.hpp>
 #include <gh/detail/backoff_strategy.hpp>
 #include <gh/detail/deadline_timer.hpp>
+#include <gh/detail/default_grpc_interceptor.hpp>
 #include <etcd/etcdserver/etcdserverpb/rpc.grpc.pb.h>
 #include <grpc++/grpc++.h>
 #include <mutex>
 #include <set>
+#include <sstream>
 #include <string>
 
 namespace gh {
@@ -15,37 +18,57 @@ namespace detail {
 /**
  * Keep track of a etcd cluster membership.
  *
- * @tparam completion_queue_type a dependency injection point to mock the completion queue.
+ * @tparam completion_queue_interceptor_type a dependency injection point to control the type of completion queue,
+ * mostly used in testing/mocking.
  */
-template <typename completion_queue_type>
+template <typename completion_queue_interceptor_type = gh::detail::default_grpc_interceptor>
 class cluster_membership_impl {
 public:
+  //@{
+  /// @name type traits
+
+  /// The type of completion queue based on the interceptor type.
+  using completion_queue_type = gh::completion_queue<completion_queue_interceptor_type>;
+
+  /// The asynchronous operation used to request the list of members.
+  using member_list_op = gh::detail::async_rpc_op<etcdserverpb::MemberListRequest, etcdserverpb::MemberListResponse>;
+  //@}
+
+  /// The minimum interval between refresh attempts.
+  static std::chrono::milliseconds constexpr min_refresh_interval{5000};
+
+  template <typename duration_type>
   cluster_membership_impl(
       completion_queue_type& queue, std::shared_ptr<grpc::ChannelCredentials> credentials,
-      std::shared_ptr<backoff_strategy> backoff, std::string const& url)
+      std::shared_ptr<backoff_strategy> backoff, std::string const& url, duration_type refresh_interval)
       : queue_(queue)
       , credentials_(std::move(credentials))
-      , backoff_strategy_(backoff)
-      , mu_()
-      , member_urls_() {
+      , backoff_strategy_(std::move(backoff))
+      , refresh_interval_(std::chrono::duration_cast<std::chrono::microseconds>(refresh_interval)) {
     member_urls_.insert(url);
+    validate_arguments();
   }
 
+  template <typename duration_type>
   cluster_membership_impl(
       completion_queue_type& queue, std::shared_ptr<grpc::ChannelCredentials> credentials,
-      std::shared_ptr<backoff_strategy> backoff,
-      std::initializer_list<std::string>&& l)
+      std::shared_ptr<backoff_strategy> backoff, std::initializer_list<std::string>&& l, duration_type refresh_interval)
       : queue_(queue)
       , credentials_(std::move(credentials))
-      , backoff_strategy_(backoff)
-      , mu_()
-      , member_urls_(l) {
+      , backoff_strategy_(std::move(backoff))
+      , member_urls_(l)
+      , refresh_interval_(std::chrono::duration_cast<std::chrono::microseconds>(refresh_interval)) {
+    validate_arguments();
   }
 
   void startup() {
-    auto delay = backoff_strategy_->current_delay();
-    current_timer_ = queue_.make_relative_timer(
-        delay, "cluster_membership/timer", [this](auto const& op, bool ok) { this->on_refresh_expired(ok); });
+    reset_refresh_timer();
+  }
+
+  void shutdown() {
+    if (refresh_timer_) {
+      refresh_timer_->cancel();
+    }
   }
 
   std::vector<std::string> current_urls() const {
@@ -54,7 +77,18 @@ public:
   }
 
 private:
+  void validate_arguments() {
+    using namespace std::chrono;
+    if (refresh_interval_ <= min_refresh_interval) {
+      std::ostringstream os;
+      os << "cluster_membership_impl<>() - refresh interval (" << duration_cast<milliseconds>(refresh_interval_).count()
+         << "ms) too short, should be >= " << min_refresh_interval.count() << "ms";
+      throw std::invalid_argument(os.str());
+    }
+  }
+
   void on_refresh_expired(bool ok) {
+    refresh_timer_.reset();
     if (not ok) {
       return;
     }
@@ -64,6 +98,7 @@ private:
 
   void try_query_cluster_membership(std::vector<std::string> urls, std::size_t i) {
     if (i >= urls.size()) {
+      backoff_strategy_->record_failure();
       return reset_refresh_timer();
     }
     auto channel = grpc::CreateChannel(urls[i], credentials_);
@@ -73,8 +108,6 @@ private:
         cluster.get(), &etcdserverpb::Cluster::Stub::AsyncMemberList, std::move(req), "cluster_membership/member_list",
         [ this, u = std::move(urls), i ](auto const& op, bool ok) { this->on_member_list_response(u, i, op, ok); });
   }
-
-  using member_list_op = gh::detail::async_rpc_op<etcdserverpb::MemberListRequest, etcdserverpb::MemberListResponse>;
 
   void on_member_list_response(std::vector<std::string> urls, std::size_t i, member_list_op const& rop, bool rok) {
     if (not rok or not rop.status.ok()) {
@@ -92,17 +125,14 @@ private:
     std::lock_guard<std::mutex> lock(mu_);
     member_urls_.swap(new_urls);
     backoff_strategy_->record_success();
+    reset_refresh_timer();
   }
 
   void reset_refresh_timer() {
-    auto delay = record_failure();
-    current_timer_ = queue_.make_relative_timer(
-        delay, "cluster_membership/timer", [this](auto& op, bool ok) { this->on_refresh_expired(ok); });
-  }
-
-  std::chrono::milliseconds record_failure() {
-    std::lock_guard<std::mutex> lock(mu_);
-    return backoff_strategy_->record_failure();
+    refresh_timer_ =
+        queue_.make_relative_timer(refresh_interval_, "cluster_membership/refresh_timer", [this](auto& op, bool ok) {
+          this->on_refresh_expired(ok);
+        });
   }
 
 private:
@@ -112,8 +142,12 @@ private:
 
   mutable std::mutex mu_;
   std::set<std::string> member_urls_;
-  std::shared_ptr<deadline_timer> current_timer_;
+  std::chrono::microseconds refresh_interval_;
+  std::shared_ptr<deadline_timer> refresh_timer_;
 };
+
+template <typename completion_queue_interceptor_type>
+std::chrono::milliseconds constexpr cluster_membership_impl<completion_queue_interceptor_type>::min_refresh_interval;
 } // namespace detail
 } // namespace gh
 
--- gh/detail/cluster_membership_impl_ut.cpp
+++ gh/detail/cluster_membership_impl_ut.cpp
@@ -1,6 +1,5 @@
 #define GH_MIN_SEVERITY trace
 #include "gh/detail/cluster_membership_impl.hpp"
-#include <gh/completion_queue.hpp>
 #include <gh/detail/exponential_backoff.hpp>
 #include <gh/detail/mocked_grpc_interceptor.hpp>
 
@@ -8,17 +7,151 @@
 
 /// Define helper types and functions used in these tests
 namespace {
-using completion_queue_type = gh::completion_queue<gh::detail::mocked_grpc_interceptor>;
-using cluster_membership_type = gh::detail::cluster_membership_impl<completion_queue_type>;
-}
+using interceptor_type = gh::detail::mocked_grpc_interceptor;
+using completion_queue_type = gh::completion_queue<interceptor_type>;
+using cluster_membership_type = gh::detail::cluster_membership_impl<interceptor_type>;
+} // anonymous namespace
 
-/// @test Verify that we can construct a gh::detail::cluster_membership<> object.
-TEST(cluster_membership_impl, basic) {
+
+/// @test Verify that cluster membership works correctly in the normal lifecycle.
+TEST(cluster_membership_impl, normal_lifecycle) {
+  using namespace std::chrono_literals;
+  using namespace gh::detail;
+  using namespace testing;
+
   using namespace std::chrono_literals;
   completion_queue_type queue;
   std::shared_ptr<grpc::ChannelCredentials> credentials;
   auto backoff = std::make_shared<gh::detail::exponential_backoff>(10ms, 24h, 1000);
-  cluster_membership_type cluster(queue, credentials, backoff, "localhost:22379");
+  cluster_membership_type cluster(queue, credentials, backoff, "localhost:22379", 30s);
+
+  std::shared_ptr<deadline_timer> pending_timer;
+  auto handle_timer = [r = std::ref(pending_timer)](auto bop) {
+    auto* op = dynamic_cast<deadline_timer*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    // ... the standard trick to downcast shared_ptr<> ...
+    r.get() = std::shared_ptr<deadline_timer>(bop, op);
+  };
+  EXPECT_CALL(*queue.interceptor().shared_mock, make_deadline_timer(Truly([](auto op) {
+    return op->name == "cluster_membership/refresh_timer";
+  }))).WillRepeatedly(Invoke(handle_timer));
+
+  // ... we expect a call to request a lease ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  }))).WillOnce(Invoke([](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+
+    // ... in this test we just assume everything worked, so provide a response ...
+    auto& m0 = *op->response.add_members();
+    *m0.add_clienturls() = std::string("url0.0");
+    *m0.add_clienturls() = std::string("url0.1");
+    auto& m1 = *op->response.add_members();
+    *m1.add_clienturls() = std::string("url1.0");
+    *m1.add_clienturls() = std::string("url1.1");
+    // ... and to not forget the callback ...
+    bop->callback(*bop, true);
+  }));
 
   EXPECT_NO_THROW(cluster.startup());
+  auto pt = std::move(pending_timer);
+  ASSERT_TRUE(pt);
+  pt->callback(*pt, true);
+
+  auto urls = cluster.current_urls();
+  EXPECT_THAT(urls, ElementsAre("url0.0", "url0.1", "url1.0", "url1.1"));
+
+  EXPECT_NO_THROW(cluster.shutdown());
+}
+
+/// @test Verify that cluster membership works correctly even with some failures.
+TEST(cluster_membership_impl, with_failure) {
+  using namespace std::chrono_literals;
+  using namespace gh::detail;
+  using namespace testing;
+
+  using namespace std::chrono_literals;
+  completion_queue_type queue;
+  std::shared_ptr<grpc::ChannelCredentials> credentials;
+  auto backoff = std::make_shared<gh::detail::exponential_backoff>(10ms, 24h, 1000);
+  cluster_membership_type cluster(queue, credentials, backoff, "localhost:22379", 30s);
+
+  std::shared_ptr<deadline_timer> pending_timer;
+  auto handle_timer = [r = std::ref(pending_timer)](auto bop) {
+    auto* op = dynamic_cast<deadline_timer*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    // ... the standard trick to downcast shared_ptr<> ...
+    r.get() = std::shared_ptr<deadline_timer>(bop, op);
+  };
+  EXPECT_CALL(*queue.interceptor().shared_mock, make_deadline_timer(Truly([](auto op) {
+    return op->name == "cluster_membership/refresh_timer";
+  }))).WillRepeatedly(Invoke(handle_timer));
+
+  // ... we expect a call to request a lease ...
+  EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  }))).WillOnce(Invoke([](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+
+    // ... in this test we just assume everything worked, so provide a response ...
+    auto& m0 = *op->response.add_members();
+    *m0.add_clienturls() = std::string("url0.0");
+    *m0.add_clienturls() = std::string("url0.1");
+    *m0.add_clienturls() = std::string("url0.2");
+    // ... and to not forget the callback ...
+    bop->callback(*bop, true);
+  }));
+
+  EXPECT_NO_THROW(cluster.startup());
+  auto pt = std::move(pending_timer);
+  ASSERT_TRUE(pt);
+  pt->callback(*pt, true);
+
+  auto urls = cluster.current_urls();
+  EXPECT_THAT(urls, ElementsAre("url0.0", "url0.1", "url0.2"));
+
+  // ... simulate some failures in the second iteration, first a canceled operation ...
+  auto& call0 = EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  })));
+  call0.WillOnce(Invoke([](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    bop->callback(*bop, false);
+  }));
+
+  // ... then some kind of failure ...
+  auto& call1 = EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  })));
+  call1.After(call0).WillOnce(Invoke([](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    op->status = grpc::Status(grpc::DEADLINE_EXCEEDED, "too long", "really, I mean it.");
+    bop->callback(*bop, true);
+  }));
+
+  // ... then some kind of failure ...
+  auto& call2 = EXPECT_CALL(*queue.interceptor().shared_mock, async_rpc(Truly([](auto op) {
+    return op->name == "cluster_membership/member_list";
+  })));
+  call2.After(call1).WillOnce(Invoke([](auto bop) {
+    using op_type = cluster_membership_type::member_list_op;
+    auto* op = dynamic_cast<op_type*>(bop.get());
+    ASSERT_TRUE(op != nullptr);
+    op->status = grpc::Status(grpc::UNKNOWN, "something bad", "without more details.");
+    bop->callback(*bop, true);
+  }));
+
+  pt = std::move(pending_timer);
+  ASSERT_TRUE(pt);
+  pt->callback(*pt, true);
+
+  EXPECT_NO_THROW(cluster.shutdown());
 }
