e976031679fe63e958abe2e0894e6c616d2a7703
Carlos O'Ryan <coryan@users.noreply.github.com>
2018-02-19 12:30:57 -0500

    Remove usage of LINUX_BUILD variable.
    The variable is no longer set, we just rely on TRAVIS_OS_NAME.

--- ci/install-linux.sh
+++ ci/install-linux.sh
@@ -7,15 +7,10 @@ if [ "${TRAVIS_OS_NAME}" != "linux" ]; then
     exit 0
 fi
 
-# Update docker, the default version installed in Travis is 17.04, just shy of what we need (17.06).
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo docker --version
-
 # Restore the docker image from the cached directory.  That way we can reuse the steps in the docker image that
 # install pre-requisites and build dependencies ...
 TARBALL=docker-images/${DISTRO?}/${DISTRO_VERSION?}/saved.tar.gz
-if [ "${LINUX_BUILD?}" = "yes" -a -f ${TARBALL?} ]; then
+if [ -f ${TARBALL?} ]; then
     gunzip <${TARBALL?} | sudo docker load || echo "Could not load saved image, continue without cache";
 fi
 
